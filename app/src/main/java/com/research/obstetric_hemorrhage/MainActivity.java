1855b984cfb15af2cd263b334fc72764311b6eb8
Brian D. Torres <dtdev98@gmail.com>
2019-09-23 15:29:25 -0400

    Fragments Added

--- app/src/main/java/com/research/obstetric_hemorrhage/MainActivity.java
+++ app/src/main/java/com/research/obstetric_hemorrhage/MainActivity.java
@@ -1,17 +1,19 @@
 package com.research.obstetric_hemorrhage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.MenuItem;
 
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
                 updateUI(null);
             }
         });*/
+
+        BottomNavigationView Nav = (BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        Nav.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new Patient_Fragment());
     }
 
     public void updateUI(FirebaseUser user){
@@ -41,5 +48,38 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed()
     {
         moveTaskToBack(true);
+    }
+
+    private boolean loadFragment(Fragment fragment)
+    {
+        if(fragment != null){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.FramePatients, fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment = null;
+
+         switch (menuItem.getItemId()){
+             case R.id.Patients:
+                 fragment = new Patient_Fragment();
+                 break;
+             case R.id.APatient:
+                 fragment = new Actual_Patient();
+                 break;
+             case R.id.Add:
+                 fragment = new add_patient();
+                 break;
+         }
+
+        return loadFragment(fragment);
     }
 }
