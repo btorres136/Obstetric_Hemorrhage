6f02988385f4136ea41c23afc80185f0b845c527
sebascasil13 <sebastian13casillas@gmail.com>
2019-10-03 16:57:44 -0400

    patient info layout added

---
+++ app/src/main/java/com/research/obstetric_hemorrhage/patient_info.java
@@ -0,0 +1,14 @@
+package com.research.obstetric_hemorrhage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class patient_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_info_layout);
+    }
+}
