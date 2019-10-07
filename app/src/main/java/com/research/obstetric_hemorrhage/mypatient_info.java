b3c51c90d4ca9d37bc144d87f31a54aafc3fee36
sebascasil13 <sebastian13casillas@gmail.com>
2019-10-07 17:56:37 -0400

    mypatientinfo .java and .xml was added

---
+++ app/src/main/java/com/research/obstetric_hemorrhage/mypatient_info.java
@@ -0,0 +1,14 @@
+package com.research.obstetric_hemorrhage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mypatient_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypatient_info_layout);
+    }
+}
