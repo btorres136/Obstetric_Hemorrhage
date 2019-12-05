5b66ab3573d109b6f214879ad0839a1f246a079d
sebascasil13 <sebastian13casillas@gmail.com>
2019-12-05 04:01:00 -0400

    mypatientinfo_layout

---
+++ app/src/main/java/com/research/obstetric_hemorrhage/input_data.java
@@ -0,0 +1,14 @@
+package com.research.obstetric_hemorrhage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class input_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_data);
+    }
+}
