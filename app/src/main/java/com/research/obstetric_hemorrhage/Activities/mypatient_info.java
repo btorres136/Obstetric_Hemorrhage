824f09a68dab211bfaaa8a8c6189b7ebce5c3afe
btorres136 <dtdev98@gmail.com>
2019-12-05 21:50:04 -0400

    Faster DataTransfer

--- app/src/main/java/com/research/obstetric_hemorrhage/Activities/mypatient_info.java
+++ app/src/main/java/com/research/obstetric_hemorrhage/Activities/mypatient_info.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -31,8 +32,18 @@ public class mypatient_info extends AppCompatActivity {
     EditText sis_press;
     EditText dis_press;
     EditText ebl;
-    EditText perfusion;
-    EditText mental;
+    //EditText perfusion;
+    //EditText mental;
+    CheckBox men_normal;
+    CheckBox men_agitated;
+    CheckBox men_Lethargic;
+    CheckBox men_unconsicious;
+    CheckBox per_normal;
+    CheckBox per_pallor;
+    CheckBox per_coldness;
+    CheckBox per_sweating;
+    CheckBox capillary;
+
     EditText hr;
     CardView update_infoCV;
     DatabaseTransactions db = new DatabaseTransactions();
@@ -48,8 +59,16 @@ public class mypatient_info extends AppCompatActivity {
         sis_press = findViewById(R.id.sis_press);
         dis_press = findViewById(R.id.dis_press);
         ebl = findViewById(R.id.ebl);
-        perfusion = findViewById(R.id.perfusion);
-        mental = findViewById(R.id.mental);
+        men_normal = findViewById(R.id.normal);
+        men_Lethargic = findViewById(R.id.lethargic);
+        men_agitated = findViewById(R.id.agitated);
+        men_unconsicious = findViewById(R.id.unconscious);
+        per_normal = findViewById(R.id.normal1);
+        per_pallor = findViewById(R.id.pallor);
+        per_coldness = findViewById(R.id.coldness);
+        per_sweating = findViewById(R.id.sweating);
+        capillary = findViewById(R.id.capillary);
+
         hr = findViewById(R.id.hr);
         update_infoCV = findViewById(R.id.update_infoCV);
         String PatientID = getIntent().getExtras().getString("PATIENT_ID");
@@ -91,24 +110,23 @@ public class mypatient_info extends AppCompatActivity {
                 String sis = sis_press.getText().toString();
                 String dis = dis_press.getText().toString();
                 String ebls = ebl.getText().toString();
-                String perf = perfusion.getText().toString();
-                String mentals = mental.getText().toString();
+                //String perf = perfusion.getText().toString();
+                //String mentals = mental.getText().toString();
                 String hrs = hr.getText().toString();
                 sis_press.getText().clear();
                 dis_press.getText().clear();
                 ebl.getText().clear();
-                perfusion.getText().clear();
-                mental.getText().clear();
+                //perfusion.getText().clear();
+                //mental.getText().clear();
                 hr.getText().clear();
-                if(sis.isEmpty() || dis.isEmpty() || ebls.isEmpty() || perf.isEmpty() ||
-                mentals.isEmpty() || hrs.isEmpty()){
+                if(sis.isEmpty() || dis.isEmpty() || ebls.isEmpty() || hrs.isEmpty()){
                     Toast.makeText(view.getContext(), "Please fill all the fields", Toast.LENGTH_LONG).show();
                 }else{
                     DecimalFormat df = new DecimalFormat("###.###");
                     double shock = (Double.parseDouble(hrs)/Double.parseDouble(sis));
                     final MediaPlayer md = MediaPlayer.create(view.getContext(), R.raw.alert);
                     databaseTransactions.addPatientinfo(getIntent().getExtras().getString("PATIENT_ID"),ebls,"0",
-                            dis,hrs,mentals,perf,df.format(shock),sis);
+                            dis,hrs,"0","0",df.format(shock),sis);
 
                     if (Integer.parseInt(sis) >= 90)
                     {
