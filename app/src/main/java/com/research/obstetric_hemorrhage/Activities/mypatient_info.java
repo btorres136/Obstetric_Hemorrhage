2d59c488a5b1af5e5f7e20d28fc99bb64fa92eed
sebascasil13 <sebastian13casillas@gmail.com>
2019-12-05 23:00:57 -0400

    andelewey

--- app/src/main/java/com/research/obstetric_hemorrhage/Activities/mypatient_info.java
+++ app/src/main/java/com/research/obstetric_hemorrhage/Activities/mypatient_info.java
@@ -178,6 +178,10 @@ public class mypatient_info extends AppCompatActivity {
                             dis,hrs,men,per,df.format(shock),sis);
 
                     if ((Integer.parseInt(sis) >= 90))
+                        if(500>Integer.parseInt(ebls) <999)
+                    {
+
+                    }
                     {
                         AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext(), R.style.green);
                         builder.setTitle("Stage 0");
